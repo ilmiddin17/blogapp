fc18ce5eb8341bf0410d22aec5e5bcc860613e60
Ilmiddin Tuychiev <ilmiddin17@mail.ru>
2021-11-11 15:58:36 +0500

    again worked on pg_trgm.py

--- blog/migrations/pg_trgm.py
+++ blog/migrations/pg_trgm.py
@@ -2,7 +2,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('myapp', <0001_initial.py>),
+        ('myapp', 0001_initial.py),
     ]
     operations = [
         migrations.RunSQL('CREATE EXTENSION IF NOT EXISTS pg_trgm'),
